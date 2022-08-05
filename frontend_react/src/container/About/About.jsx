36480b307bd107d60bd231d752c3aa17d8448249
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-08-05 16:41:07 +0600

    backend implemented and about section has been updated

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,40 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
-import { images } from "../../constants";
-
 import "./About.scss";
-
-const abouts = [
-  {
-    title: "Frontend Development",
-    description: "I am a good frontend developer.",
-    imgURL: images.about01,
-  },
-  {
-    title: "Backend Development",
-    description: "I am a good backend developer.",
-    imgURL: images.about02,
-  },
-  {
-    title: "Modern Web Design",
-    description: "I am a good modern web designer.",
-    imgURL: images.about03,
-  },
-  {
-    title: "Web Animations",
-    description: "I am very skillfull at the web animations.",
-    imgURL: images.about04,
-  },
-];
+import { urlFor, client } from "../../client";
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
   return (
     <>
       <h2 className="head-text">
-        I Know That <span>Good Apps</span>
-        <br />
-        Means <span>Good Business</span>
+        I Know that <span>Good Design</span> <br />
+        means <span>Good Business</span>
       </h2>
 
       <div className="app__profiles">
@@ -46,7 +31,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgURL} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>

45f730833cca1f98cf3e033637968b7a1f6946d6
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-08-06 12:55:50 +0600

    portfolio filters added

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+import { AppWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
 
 import "./Work.scss";
 
 const Work = () => {
-  return <div>Work</div>;
+  const [activeFilter, setActiveFilter] = useState("All");
+  const handleWorkFilter = (item) => {};
+
+  return (
+    <>
+      <h2 className="head-text">
+        My Creative <span>Portfolio Showcase</span>
+      </h2>
+
+      <div className="app__work-filter">
+        {["React Apps", "Web Apps", "Mobile Apps", "UI/UX"].map(
+          (item, index) => (
+            <div
+              key={index}
+              onClick={() => handleWorkFilter(item)}
+              className={`app__work-filter-item app__flex p-tex ${
+                activeFilter === item ? "item-active" : ""
+              }`}
+            >
+              {item}
+            </div>
+          )
+        )}
+      </div>
+
+      <motion.div></motion.div>
+    </>
+  );
 };
 
 export default Work;
